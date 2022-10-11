261f07a5436d69a49abbd7557dad596a293315af
ChengXiangdong <chengxiangdong@outlook.com>
2022-10-11 16:57:50 +0800

    Create run-e2e.sh

---
+++ hack/run-e2e.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+ARTIFACTS_PATH=${ARTIFACTS_PATH:-"${HOME}/e2e-logs"}
+mkdir -p "$ARTIFACTS_PATH"
+
+# Install ginkgo
+go install -mod=mod github.com/onsi/ginkgo/v2/ginkgo@latest
+
+# Run e2e
+export GOLANG_PROTOBUF_REGISTRATION_CONFLICT=warn
+
+set +e
+ginkgo -v --race --trace --fail-fast -p --randomize-all ./test/e2e/
+TESTING_RESULT=$?
+
+# Collect logs
+echo "Collected logs at $ARTIFACTS_PATH:"
+ls -al "$ARTIFACTS_PATH"
+
+exit $TESTING_RESULT
