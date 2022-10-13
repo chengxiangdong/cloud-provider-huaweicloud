9ab5105d5a2ee764b1b905595c6f5e1785a542d6
ChengXiangdong <chengxiangdong@outlook.com>
2022-10-13 09:46:34 +0800

    add e2e test

--- hack/run-e2e.sh
+++ hack/run-e2e.sh
@@ -6,15 +6,16 @@ set -o pipefail
 
 kubectl version
 
-ARTIFACTS_PATH=${ARTIFACTS_PATH:-"${HOME}/e2e-logs"}
-mkdir -p "$ARTIFACTS_PATH"
+REPO_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
+
+ARTIFACTS_PATH=${ARTIFACTS_PATH:-"${REPO_ROOT}/e2e-logs"}
+mkdir -p "${ARTIFACTS_PATH}"
 
 # Install ginkgo
 go install -mod=mod github.com/onsi/ginkgo/v2/ginkgo@v2.2.0
 
 # Pre run e2e for extra components
 echo "Run pre run e2e"
-REPO_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
 sh "${REPO_ROOT}"/hack/pre-run-e2e.sh
 
 # Run e2e
@@ -26,8 +27,9 @@ ginkgo -v --race --trace --fail-fast -p --randomize-all ./test/e2e/
 TESTING_RESULT=$?
 
 # todo(chengxiangdong): Collect logs
-echo "Collected logs at $ARTIFACTS_PATH:"
-ls -al "$ARTIFACTS_PATH"
+kubectl logs daemonset/huawei-cloud-controller-manager -n kube-system > ${ARTIFACTS_PATH}/huawei-cloud-controller-manager.log
+echo "Collected logs at ${ARTIFACTS_PATH}:"
+ls -al "${ARTIFACTS_PATH}"
 
 # Post run e2e for delete extra components
 echo "Run post run e2e"
