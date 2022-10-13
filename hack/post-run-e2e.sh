897c240e62ed337e4672ebd5e07461de3a1ffa19
ChengXiangdong <chengxiangdong@outlook.com>
2022-10-13 09:46:34 +0800

    add e2e test

---
+++ hack/post-run-e2e.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+kubectl delete -n kube-system daemonset huawei-cloud-controller-manager
+kkubectl delete -n kube-system serviceaccount cloud-controller-manager
+kubectl delete clusterrole ccm-secret-role
+kubectl delete clusterrolebinding ccm-secret-binding
+kubectl delete clusterrolebinding extension-apiserver-auth-reader-binding
